cd5c95c82e71ce1fba0e11820772ae81e012152d
Kendallfallassss <kendfb07@gmail.com>
2025-08-13 23:13:59 -0600

    ladklakd

--- HotelCostaAzul.API/Program.cs
+++ HotelCostaAzul.API/Program.cs
@@ -1,17 +1,37 @@
+using HotelCostaAzul.API.Services;
+using HotelCostaAzul.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IRolesService, RolesService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IOpinionService, OpinionService>();
+builder.Services.AddScoped<IHotelService, HotelService>();
+builder.Services.AddScoped<IMetodoPagoService, MetodoPagoService>();
+builder.Services.AddScoped<IHabitacionService, HabitacionService>();
+builder.Services.AddScoped<IReservacionService, ReservacionService>();
+builder.Services.AddScoped<IPagoService, PagoService>();
+builder.Services.AddScoped<IFacturacionService, FacturacionService>();
+builder.Services.AddScoped<IDetalleFacturaService, DetalleFacturaService>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<Hotelify.API.Services.HashingService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<HotelifyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
--- HotelCostaAzulShare/Models/Entities/Role.cs
+++ HotelCostaAzulShare/Models/Entities/Role.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HotelCostaAzul.API.Models;
-
-public partial class Role
+namespace HotelCostaAzul.API.Models.Entities
 {
-    public int Id { get; set; }
+    public class Roles
+    {
+        [Key]
+        public int Id { get; set; }
 
-    public string Tipo { get; set; } = null!;
+        [Required]
+        [RegularExpression("^(Administrador|Cliente)$", ErrorMessage = "El nombre debe ser 'Admin' o 'User'.")]
+        public string Tipo { get; set; }
 
-    public string? Descripcion { get; set; }
+        public string? Descripcion { get; set; }
+    }
 }
